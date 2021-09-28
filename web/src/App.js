0935e31898ca43bbd0e013e84baf4062653c0b59
Zoopsoup <bzijiexia@gmail.com>
2021-09-28 11:34:30 -0500

    added lesson module component

--- web/src/App.js
+++ web/src/App.js
@@ -1,53 +1,33 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Navbar, Container, Nav, Card } from 'react-bootstrap';
 import { Switch, Route, Link, useHistory } from 'react-router-dom';
+import LessonModule from './Components/LessonModule';
+import sharinginfo from './sharinginfo.png';
 
 function App() {
-  // const history = useHistory();
-  return (
-    <div className="App">
-      <Navbar bg="dark" variant="dark">
-        <Container>
-          <Navbar.Collapse>
-            <Navbar.Brand href="#home">
-              <img
-                alt=""
-                src="logo192.png"
-                width="30"
-                height="30"
-                className="d-inline-block align-top"
-              />
-              I-CCEW SoBA 2021
-            </Navbar.Brand>
-            <Nav className="me-auto">
-              <Nav.Link>Home</Nav.Link>
-              <Nav.Link>{/* <Link to="/features">Features</Link> */}</Nav.Link>
-              <Nav.Link>{/* <Link to="/pricing">Pricing</Link> */}</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-      <div>
-        <Switch>
-          <Route path="/features">
-            <Card>
-              <Card.Body>Features Route</Card.Body>
-            </Card>
-          </Route>
-          <Route path="/pricing">
-            <Card>
-              <Card.Body>Pricing Route</Card.Body>
-            </Card>
-          </Route>
-          <Route path="/">
-            <Card>
-              <Card.Body>Home Route</Card.Body>
-            </Card>
-          </Route>
-        </Switch>
-      </div>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Navbar bg="dark" variant="dark">
+				<Container>
+					<Navbar.Collapse>
+						<Navbar.Brand href="#home">
+							<img alt="" src="logo192.png" width="30" height="30" className="d-inline-block align-top" />
+							I-CCEW SoBA 2021
+						</Navbar.Brand>
+						<Nav className="me-auto">
+							<Nav.Link>Home</Nav.Link>
+							<Nav.Link>{/* <Link to="/features">Features</Link> */}</Nav.Link>
+							<Nav.Link>{/* <Link to="/pricing">Pricing</Link> */}</Nav.Link>
+						</Nav>
+					</Navbar.Collapse>
+				</Container>
+			</Navbar>
+			<div className="centered-div">
+				<LessonModule label="Sharing Information" imageLink={sharinginfo} />
+				<LessonModule label="Information" imageLink={sharinginfo} />
+			</div>
+		</div>
+	);
 }
 
 export default App;
