106c8cf29bd0d9979704ab576f9350afadcf3d65
Edward <>
2021-09-28 16:04:28 -0500

    added button

--- web/src/App.js
+++ web/src/App.js
@@ -1,6 +1,7 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Navbar, Container, Nav, Card } from "react-bootstrap";
 import { Switch, Route, Link, useHistory } from "react-router-dom";
+import styled from "styled-components";
 
 function App() {
   // const history = useHistory();
@@ -17,7 +18,7 @@ function App() {
                 height="30"
                 className="d-inline-block align-top"
               />
-              I-CCEW SoBA 2021
+              ASD App
             </Navbar.Brand>
             <Nav className="me-auto">
               <Nav.Link>Home</Nav.Link>
@@ -49,5 +50,36 @@ function App() {
     </div>
   );
 }
+
+const Button = styled.button`
+  background-color: ${(props) => theme[props.theme].default};
+  color: white;
+  padding: 5px 15px;
+  border-radius: 5px;
+  outline: 0;
+  text-transform: uppercase;
+  margin: 10px 0px;
+  cursor: pointer;
+  box-shadow: 0px 2px 2px lightgray;
+  transition: ease background-color 250ms;
+  &:hover {
+    background-color: ${(props) => theme[props.theme].hover};
+  }
+  &:disabled {
+    cursor: default;
+    opacity: 0.7;
+  }
+`;
+
+function clickMe() {
+  alert("You clicked me!");
+}
+
+const theme = {
+  blue: {
+    default: "#3f51b5",
+    hover: "#283593",
+  },
+};
 
 export default App;
