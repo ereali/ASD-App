07a1691bef0f86d4f4f383589392d307a1497d8c
Marcos Bernier <marcosbernier@Marcoss-MacBook-Air.local>
2021-10-02 19:48:35 -0500

    fixed

--- web/src/Components/Button.jsx
+++ web/src/Components/Button.jsx
@@ -7,6 +7,10 @@ const Button = styled.button`
 background-color:${props.color};
 borderRadius: radius;
 color:${props.textColor};
+transition: ease background-color 250ms;
+&:hover {
+ background-color: #1c73ff;    //thx edward
+;}
 `;
 
   return (
