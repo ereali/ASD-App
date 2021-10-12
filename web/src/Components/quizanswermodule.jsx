a25b4620d0a98d997869ea39f512f7a6ee9985a5
Edward <>
2021-10-12 14:29:17 -0500

    added quiz module

---
+++ web/src/Components/quizanswermodule.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+import QuizButton from "./quizbutton";
+
+function FourButtonModule(props) {
+  const FourButtonModule = styled.div`
+    <div>
+
+  `;
+}
--- web/src/Components/quizbutton.jsx
+++ web/src/Components/quizbutton.jsx
@@ -1,45 +1,28 @@
 import React from "react";
 import styled from "styled-components";
 
-function Label(props) {
-    const Label = styled.div`
-      background-color: ${props.backgroundColor};
-      color: ${props.textColor};
-      border-radius: 17px;
-      text-align: center;
-      vertical-align: center;
-      display: inline-block;
-      padding: 15px;
-      font-size: 40px;
-      margin: auto;
-    `;
-  
-    return <Label>{props.name}</Label>;
-  }
+function QuizButton(props) {
+  const QuizButton = styled.div`
+    background-color: ${props.backgroundColor};
+    color: ${props.textColor};
+    padding: 5px 15px;
+    border-radius: 17px;
+    outline: 0;
+    margin: 10px 0px;
+    cursor: pointer;
+    box-shadow: 0px 2px 2px lightgray;
+    transition: ease background-color 250ms;
+    font-size: ${props.fontSize};
+    &:hover {
+      background-color: ${props.hoverBackgroundColor};
+    }
+    &:disabled {
+      cursor: default;
+      opacity: 0.7;
+    }
+  `;
 
-  function QuizButton(props) {
-      const QuizButton = styled.div`
-        background-color: ${props.backgroundColor};
-        color: ${props.textColor};
-        padding: 5px 15px;
-        border-radius: 17px;
-        outline: 0;
-        margin: 10px 0px;
-        cursor: pointer;
-        box-shadow: 0px 2px 2px lightgray;
-        transition: ease background-color 250ms;
-        font-size: ${props.fontSize};
-        &:hover {
-        background-color: ${props.hoverBackgroundColor};
-        }
-        &:disabled {
-        cursor: default;
-        opacity: 0.7;
-        }
-      `;
+  return <QuizButton>(props.name)</QuizButton>;
+}
 
-      return <QuizButton>(props.name)</QuizButton>;
-  }
-  
-  export default QuizButton;
-  export default Label;
+export default QuizButton;
