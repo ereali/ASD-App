7ecc81fe6ac49b631520880ef633385287c372ee
Edward <>
2021-11-09 18:55:00 -0600

    Added dnd view and updated data

--- web/src/Components/DND/dnddata.jsx
+++ web/src/Components/DND/dnddata.jsx
@@ -1,6 +1,47 @@
 export const CATEGORY = {
-  Public: "public",
-  Private: "private",
+  PUBLIC: "public",
+  PRIVATE: "private",
 };
 
-export const ANSWERS = [{}];
+export const ANSWERS = [
+  {
+    answer: "Eating a sandwich",
+    category: "public",
+  },
+  {
+    answer: "Reading a book",
+    category: "public",
+  },
+  {
+    answer: "Taking a walk",
+    category: "public",
+  },
+  {
+    answer: "Giving someone a high five",
+    category: "public",
+  },
+  {
+    answer: "Talking to a friend",
+    category: "public",
+  },
+  {
+    answer: "Using the toilet",
+    category: "private",
+  },
+  {
+    answer: "Putting on deodorant",
+    category: "private",
+  },
+  {
+    answer: "Taking off clothes",
+    category: "private",
+  },
+  {
+    answer: "Taking a shower",
+    category: "private",
+  },
+  {
+    answer: "Touching your private parts",
+    category: "private",
+  },
+];
